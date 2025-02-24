ba1019f474eaccceb5855c14a930e19a4765d5ae
kai <137367860+Itzsoham@users.noreply.github.com>
2025-02-24 23:47:54 +0530

    overview route and override html files

--- index.js
+++ index.js
@@ -1,27 +1,93 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-// Blocking, synchronous way
-const testInput = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(testInput);
+const replaceTemplate = require("./modules/replaceTemplate");
 
-const textOutput = `This is what we know about the avocado: ${testInput}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOutput);
-console.log("File has been written!");
+////////////////////////////
+// FILES
 
-// Non-blocking, asynchronous way
-// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-//   console.log(data);
+// // Blocking, synchronous way
+// const testInput = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(testInput);
+
+// const textOutput = `This is what we know about the avocado: ${testInput}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOutput);
+// console.log("File has been written!");
+
+// // Non-blocking, asynchronous way
+// // fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+// //   console.log(data);
+// // });
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("File has been written!");
+//       });
+//     });
+//   });
 // });
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("File has been written!");
-      });
+
+// console.log("Reading file...");
+
+////////////////////////////
+// SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  const pathName = req.url;
+
+  // Overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    console.log;
+    res.end(output);
+
+    // Product page
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+
+    // API
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(dataObj);
+
+    // Not found
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
     });
-  });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
-console.log("Reading file...");
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
